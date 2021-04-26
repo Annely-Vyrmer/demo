7239203094ce91ade50d81df634c64ddcc476c84
Annely.Vyrmer <annely.vurmer@gmail.com>
2021-04-26 11:56:07 +0300

    2021-04-26 commit

--- src/main/java/ee/bcs/valiit/Controller/BankController4.java
+++ src/main/java/ee/bcs/valiit/Controller/BankController4.java
@@ -20,6 +20,7 @@ public class BankController4 {
 
     //CREATE ACCOUNT
     //http://localhost:8080/createAccount4/EE1555/Reino/500
+    @CrossOrigin
     @PostMapping("createAccount4/{accNumber}/{name}/{initialAmount}")
     public void createAccount4(@PathVariable("accNumber") String accountNo,
                                @PathVariable("name") String accountName,
@@ -28,24 +29,29 @@ public class BankController4 {
     }
     //LOCK ACCOUNT
     //http://localhost:8080/lock4/EE1212
+    @CrossOrigin
     @PutMapping ("lock4/{accNumber}")
     public void lock4(@PathVariable("accNumber") String accountNo){
         bankService.lock4(accountNo);
     }
     //UNLOCK ACCOUNT
     //http://localhost:8080/unlock4/EE1212
+    @CrossOrigin
     @PutMapping ("unlock4/{accNumber}")
     public void unlock4(@PathVariable("accNumber") String accountNo){
         bankService.unlock4(accountNo);
     }
+
     //GET BALANCE
     //http://localhost:8080/getBalance4/EE1555
+    @CrossOrigin
     @GetMapping("getBalance4/{accNumber}")
     public String getBalance4(@PathVariable("accNumber") String accountNo) {
         return bankService.getBalance4(accountNo);
     }
     //DEPOSIT MONEY
     //http://localhost:8080/depositMoney4/EE1555/100
+    @CrossOrigin
     @PutMapping("depositMoney4/{accNumber}/{depositAmount}")
     public String depositMoney4(@PathVariable("accNumber") String accountNo,
                               @PathVariable("depositAmount") Double deposit) {
@@ -53,6 +59,7 @@ public class BankController4 {
     }
     //WITHDRAW MONEY
     //http://localhost:8080/withdrawMoney4/EE1444/50
+    @CrossOrigin
     @GetMapping("withdrawMoney4/{accNumber}/{withdraw}")
     public String withdrawMoney4(@PathVariable("accNumber") String accountNo,
                                  @PathVariable Double withdraw) {
@@ -60,6 +67,7 @@ public class BankController4 {
     }
 
     //http://localhost:8080/transferMoney4/EE9191/EE1222/100
+    @CrossOrigin
     @GetMapping("transferMoney4/{fromAccountNo}/{toAccountNo}/{transfer}")
     public String transferMoney4(@PathVariable("fromAccountNo") String fromAccountNo,
                                  @PathVariable("toAccountNo") String toAccountNo,
